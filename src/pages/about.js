83259d57b626404551a9d8445dbb6ccb79628480
RCAbney <ryan@rcabney.com>
2019-08-13 15:12:32 -0400

    complete intro and setup rdy for posts

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,8 +4,8 @@ import Layout from '../components/layout';
 
 export default () => (
   <Layout>
-    <h1>Home</h1>
-    <p>Hello Indiana!</p>
-    <Link to="/about/">Learn About Me &rarr;</Link>
+    <h1>About Me!</h1>
+    <p>This is my personal website.</p>
+    <Link to="/">&larr; back to home.</Link>
   </Layout>
 );
